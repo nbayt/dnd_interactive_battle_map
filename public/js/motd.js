fd7f1ba0b2fd30b8864b193c7fd7b8c4ba36f3f2
Nickolas Bayt <nickbayt@gmail.com>
2020-05-28 16:01:03 -0700

    moved css from html to external .css

--- public/js/motd.js
+++ public/js/motd.js
@@ -6,6 +6,7 @@ MOTDS = [
   `'I would like to enter a frenzied rage' ~Arvid`,
   `'I think I've made my intentions VERY clear' ~Old Man Waterfall`,
   `'(Insert any quote here)' ~Literally anyone in this chat`,
+  `"It's not racism if it keeps my money in my pocket" ~Nick`,
 ];
 
 document.getElementById('motd_title').innerHTML = MOTDS[Math.floor(Math.random()*MOTDS.length)];
